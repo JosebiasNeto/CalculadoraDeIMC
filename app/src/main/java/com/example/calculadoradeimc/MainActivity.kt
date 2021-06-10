31460ca1b4b2e5a99244ebf3891cfe3a0a494d78
Josebias Neto <josebiasbsneto@gmail.com>
2021-06-10 17:30:04 -0300

    Set Validation is not Empty

--- app/src/main/java/com/example/calculadoradeimc/MainActivity.kt
+++ app/src/main/java/com/example/calculadoradeimc/MainActivity.kt
@@ -2,10 +2,34 @@ package com.example.calculadoradeimc
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.calculadoradeimc.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val bt_calcular = binding.btCalcular
+        val mensagem = binding.tvMensagem
+
+        bt_calcular.setOnClickListener{
+            val editPeso = binding.editPeso.text.toString()
+            val editAltura = binding.editAltura.text.toString()
+
+            if (editPeso.isEmpty()){
+                mensagem.setText("Informe o Seu Peso")
+            } else if (editAltura.isEmpty()){
+                mensagem.setText("Informe a Sua Altura")
+            } else {
+                CalculoDeIMC()
+            }
+        }
+    }
+    private fun CalculoDeIMC(){
+
     }
 }
